d97051ae1dac93b756f5383821f8076705b5e1ea
Caio Marcatti <caiomarcatti12@gmail.com>
2023-06-22 19:30:41 +0000

    Adicionado main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/codelesshub/nanogo/config/env"
+	redis "github.com/codelesshub/nanogo/config/redis"
 	"github.com/codelesshub/nanogo/config/webserver"
 )
 
@@ -9,7 +10,11 @@ func main() {
 	// Carrega o arquivo .env
 	env.LoadEnv()
 
+	redis.StartRedisQueue()
+
+	// redis.Enqueue("my_job", nil)
+	// redis.RedisQueueConsumer(tasks.NewCurlTask(), "my_job")
+
 	// Inicializa o webserver
 	webserver.StartWebServer()
 }
-//
---
+++ readme.md
@@ -0,0 +1,8 @@
+go mod init codelesshub/api-crontab
+
+get github.com/codelesshub/nanogo 
+
+.env
+.nano.go
+
+curl http://host.docker.internal:8080/healthcheck
